92e41918bfca19684d0a533d89e7295e343f47ac
Eiv0n <100576362+Eiv0n@users.noreply.github.com>
2022-05-05 18:25:34 +0300

    second commit

--- Main.java
+++ Main.java
@@ -1,15 +1,23 @@
 package com.viktor.leonov;
-import java.util.Scanner;
+
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String input = "";
-        int result;
         input = scan.nextLine();
-        int num1 = Integer.parseInt(input.substring(0,1));
-        int num2 = Integer.parseInt(input.substring(2,3));
+        int num11 = Integer.parseInt(input.substring(0,1));
+        int num22 = Integer.parseInt(input.substring(2,3));
         String singOfOperation = input.substring(1, 2);
-        Operations.fromString(singOfOperation).apply(num1 , num2); // Cannot resolve method 'apply' in 'Enum'
+        Operations[] operations = Operations.values();
+        List<Operations> list2 = Arrays.asList(operations);
+
+        Optional <Operations> operation = list2.stream().filter(s -> s.equals(singOfOperation)).findAny();
+        }
+    public BinaryOperator<Integer> getFunction() {
+        return Operations.function;
     }
-}
+    }
--- Operations.java
+++ Operations.java
@@ -1,47 +1,34 @@
 package com.viktor.leonov;
-import java.util.function.BiFunction;
 
-public enum Operations implements BiFunction<Integer, Integer, Integer> {
+import java.util.function.BinaryOperator;
 
-    MUlTIPLY ("*") {
+public enum Operations implements BinaryOperator<Integer> {
+    MULTIPLY("*" ,) {
         public Integer apply(Integer num1, Integer num2) {
             return (num1 * num2);
         }
     },
-    ADD ("+") {
+    ADD("+", ) {
         public Integer apply(Integer num1, Integer num2) {
             return (num1 + num2);
         }
     },
-    DIVISION(":") {
-        public Integer apply(Integer num1, Integer num2) {
-            return (num1 / num2);
-        }
-    },
-    SEGMENTATION("-") {
+    DIVISION(":", ) {
+            public Integer apply(Integer num1, Integer num2) {
+                return num1 / num2;
+            }
+        },
+    SEGMENTATION("-", ) {
         public Integer apply(Integer num1, Integer num2) {
             return (num1 - num2);
         }
     };
-    final String sign;
-    Operations (String sign) {
-        this.sign = sign;
-    }
-    public static Enum fromString(String signOfOperation) {
-        switch (signOfOperation) {
-            case (Operations.ADD.sign): // Constant expression required
-               return Operations.ADD;
-            break;
-            case (Operations.DIVISION.sign) : // Constant expression required
-                return Operations.DIVISION;
-            break;
-            case (Operations.SEGMENTATION.sign) : // Constant expression required
-                return Operations.SEGMENTATION;
-            break;
-            case (Operations.MUlTIPLY.sign) : // Constant expression required
-                return Operations.MUlTIPLY;
-            break;
-        }
-        return ; // хз как вернуть итоговый енам
+    public BinaryOperator <Integer> function;
+
+    public final String sign;
+
+    Operations(String sign1, BinaryOperator<Integer> function1)  {
+        this.sign = sign1;
+        this.function = function1;
     }
 }
